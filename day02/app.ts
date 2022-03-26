a9f3800f66b5ce7b405371d58e97c9eb4dd1e2fb
201811824杨 <li201811828@126.com>
2022-03-26 22:25:22 +0800

    TS编译选项

--- day02/app.ts
+++ day02/app.ts
@@ -1,3 +1,30 @@
 console.log('hello');
 console.log('2222222');
 
+let a = '22222';
+
+let bs:string = '22222'
+//1111111111
+console.log(bs);
+
+function fn(a: number,b : number) {
+    return a +b;
+}
+
+function fn2(this: Window) {
+    console.log(this)
+}
+
+let box1 = document.getElementById('box1');
+if (box1 !== null){
+    box1.addEventListener('click',function () {
+        alert('hello');
+    })
+}
+
+box1?.addEventListener('click',function () {
+    alert('hello');
+})
+
+
+
---
+++ day02/m.ts
@@ -0,0 +1,7 @@
+let aaaa:string = '111';
+console.log(aaaa);
+
+let bdf:string = '2222';
+
+console.log(bdf);
+
