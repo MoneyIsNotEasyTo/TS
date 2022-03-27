99f7bee4774322713f3a24caf01cd7ce88a7471d
201811824杨 <li201811828@126.com>
2022-03-27 18:08:11 +0800

    TS编译选项

---
+++ day03/src/index.ts
@@ -0,0 +1,20 @@
+
+//import {h1} from './m1';
+
+function f1(a: number ,b : number) {
+    return a + b;
+}
+
+console.log(f1(123,456));
+
+
+const obj = {name: '孙悟空',aeg: 33};
+console.log(obj);
+obj.aeg = 18
+console.log(obj);
+
+//console.log(h1)
+
+console.log(Promise)
+
+
---
+++ day03/webpackTo.ts
@@ -0,0 +1,17 @@
+/*
+* 1.使用webpack打包TS文件
+*     1.执行npm init -y  生成 package.json文件
+*     2.安装依赖包  npm i -D webpack webpack-cli typescript ts-loader
+*     3.配置webpack.config.js 文件
+*     4.配置tsconfig.json文件
+*     5.安装 html-webpack-plugin插件      npm i -D html-webpack-plugin
+*     6.安装 webpack-dev -server 插件    npm i -D webpack-dev-server  全清空打包文件
+*     7.npm i -D @babel/core @babel/preset-env babel-loader core-js
+*
+* */
+
+/*
+* ts中webpack的配置 参考文档
+* https://blog.csdn.net/FormAda/article/details/119482417?spm=1001.2101.3001.6650.2&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-2.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-2.pc_relevant_aa&utm_relevant_index=5
+*
+* */
